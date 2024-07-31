bed7740a82cee2aff1404bea2b309e1173eeb10e
sun <sun25555@gail.com>
2024-07-31 10:14:59 +0900

    배포연습

---
+++ src/test/test01.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    };
+  },
+  setup() {
+    
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
